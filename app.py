d04feb5e7aa02938907e63e3df9a69e349d2d38d
Jose Luis Hernandez De La Cruz <dareckluis@gmail.com>
2024-06-05 16:13:43 -0600

    Update App file

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def Home():
-    return "Alumno: José Luis Hernandez De La Cruz - Grupo: A"
+    return render_template('home.html')
 
 
 if __name__ == '__main__' : 
